dd3077212e20048639d415103e6794fb56a6950e
bemain <bemain.dev@gmail.com>
2025-06-26 12:30:50 +0200

    Overwrite the dconf with the correct container ID before applying

--- run_after_load-dconf.sh
+++ run_after_load-dconf.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
 # Load dconf from the dump file
-dconf load / < ~/.config/dconf/user.conf
+
+# Replace the placeholder with the actual ID of the distrobox 'fedora' for the Ptyxis profile
+FEDORA_CONTAINER_ID="$(podman ps -a --filter name=fedora --format '{{.ID}}' --no-trunc)"
+USER_CONF=$(sed -e "s/my-fedora-distrobox/${FEDORA_CONTAINER_ID}/" ~/.local/share/chezmoi/dot_config/dconf/user.conf)
+
+echo "Loading dconf."
+
+printf "$USER_CONF" | dconf load /
