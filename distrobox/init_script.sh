f4e6e011f7158cdd17684026b17ebe1e455b8dc5
bemain <bemain.dev@gmail.com>
2025-06-26 13:19:25 +0200

    Replace codium with code

--- distrobox/init_script.sh
+++ distrobox/init_script.sh
@@ -33,15 +33,15 @@ sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Add VSCodium repository
-sudo rpmkeys --import -y https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 # Add Chezmoi Copr repository
 sudo dnf copr enable -y lihaohong/chezmoi
 
 # Install packages
 sudo dnf check-update -y
-sudo dnf install -y codium chezmoi
+sudo dnf install -y code chezmoi
 
 # Install Flutter
 sudo mkdir /usr/local/flutter
